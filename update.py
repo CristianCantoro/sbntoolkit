e9b1766763cff8830e2ce69fa7cd5558598d1875
CristianCantoro <kikkocristian@gmail.com>
2013-12-13 17:28:58 +0100

    Read file names from config

--- update.py
+++ update.py
@@ -1,19 +1,16 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import argparse
-import logging
-import requests
 import os
+import logging
+import argparse
+import requests
+import configparser
 from csvunicode import UnicodeWriter, UnicodeReader
 from subprocess import Popen, PIPE
 
 import database
 
-WIKIPEDIA_OUTPUT = 'wp_pages.txt'
-WIKIDATA_OUTPUT = 'wd_items.txt'
-WIKIDATA_OUTPUT_TMP = 'tmp_wd_items.txt'
-UPDATE_OUTPUT = 'items_to_update.txt'
 
 FIELDS = ['lastrevid', 'pageid', 'title', 'counter', 'length',
           'contentmodel', 'pagelanguage', 'touched', 'ns']
@@ -24,6 +21,15 @@ AVAILABLE_PROPERTIES = set(['P396', 'P214', 'P244'])
 
 # logging
 logger = logging.getLogger('sbnredirect.item')
+
+
+def read_config(config_name):
+
+    config = configparser.ConfigParser()
+    config.read(config_name)
+
+    return config
+
 
 def pages_with_template(template, lang='en', geicontinue=None):
     """
@@ -40,7 +46,6 @@ def pages_with_template(template, lang='en', geicontinue=None):
         'prop': 'info',
         'format': 'json'
     }
-
 
     geic_pre = geicontinue
     logger.debug('geic_pre: {}'.format(geic_pre and geic_pre.encode('utf-8')))
@@ -59,7 +64,8 @@ def pages_with_template(template, lang='en', geicontinue=None):
     except KeyError:
         pass
 
-    logger.debug('geic_post: {}'.format(geic_post and geic_post.encode('utf-8')))
+    logger.debug('geic_post: {}'.format(
+        geic_post and geic_post.encode('utf-8')))
 
     pages = []
     try:
@@ -73,6 +79,7 @@ def pages_with_template(template, lang='en', geicontinue=None):
     if geic_post:
         for item in pages_with_template(template, lang, geic_post):
             yield item
+
 
 def get_items_with_property(property_, gblcontinue=None):
     """
@@ -110,7 +117,8 @@ def get_items_with_property(property_, gblcontinue=None):
     except KeyError:
         pass
 
-    logger.debug('gblc_post: {}'.format(gblc_post and gblc_post.encode('utf-8')))
+    logger.debug('gblc_post: {}'.format(
+        gblc_post and gblc_post.encode('utf-8')))
 
     pages = res.json()['query']['pages']
     for page in pages:
@@ -119,6 +127,7 @@ def get_items_with_property(property_, gblcontinue=None):
     if gblc_post:
         for item in get_items_with_property(property_, gblc_post):
             yield item
+
 
 def get_items_to_update(table, items):
     logger.debug('no. of items: {no}'.format(no=len(items)))
@@ -137,6 +146,7 @@ def get_items_to_update(table, items):
 
     return items_to_get
 
+
 def get_wikidata_items_from_database(properties):
     try:
         pass
@@ -145,24 +155,27 @@ def get_wikidata_items_from_database(properties):
 
     return None
 
-def _write_wikidata_file_header():
-    if not os.path.isfile(WIKIDATA_OUTPUT) or \
-           os.stat(WIKIDATA_OUTPUT).st_size == 0:
-        with open(WIKIDATA_OUTPUT, 'w') as out:
+
+def _write_wikidata_file_header(config):
+    if not os.path.isfile(config['data']['wikidata_output']) or \
+            os.stat(config['data']['wikidata_output']).st_size == 0:
+        with open(config['data']['wikidata_output'], 'w') as out:
             writer = UnicodeWriter(out)
             keys = [unicode(k) for k in FIELDS]
             writer.writerow(keys)
             out.flush()
 
-def _write_wikidata_file_items(items):    
-    with open(WIKIDATA_OUTPUT, 'a') as out:
+
+def _write_wikidata_file_items(items):
+    with open(config['data']['wikidata_output'], 'a') as out:
         writer = UnicodeWriter(out)
         for item in items:
             row = [unicode(item[k]) for k in FIELDS]
             writer.writerow(row)
         out.flush()
 
-def _get_property(p):
+
+def _get_property(p, config):
     if p in AVAILABLE_PROPERTIES:
         logger.debug('Get property {} from API'.format(p))
         items = None
@@ -173,38 +186,43 @@ def _get_property(p):
 
         if items:
             logger.debug('Writing results for {}'.format(p))
-            _write_wikidata_file_header()
+            _write_wikidata_file_header(config)
             _write_wikidata_file_items(items)
 
 
-def get_wikidata_items_from_api(properties):
+def get_wikidata_items_from_api(config, properties):
 
     if properties:
         for p in properties:
-            _get_property(p)
+            _get_property(p, config)
 
     # FIXME
     # lots of bugs in I/O
-    p1 = Popen(['sort','-r', WIKIDATA_OUTPUT, '-o', WIKIDATA_OUTPUT_TMP],
+    p1 = Popen(['sort', '-r', config['data']['wikidata_output'],
+                '-o', config['data']['wikidata_output_tmp']],
                shell=True)
-    p2 = Popen(['uniq', WIKIDATA_OUTPUT_TMP, WIKIDATA_OUTPUT],
+    p2 = Popen(['uniq', config['data']['wikidata_output_tmp'],
+                config['data']['wikidata_output']],
                shell=True)
 
-def get_wikidata_items(properties):
+
+def get_wikidata_items(config, properties):
     items = []
 
     logger.debug('Try DB')
 
     items = get_wikidata_items_from_database(properties)
-    
+
     if not items:
         logger.debug('Try API')
-        get_wikidata_items_from_api(properties)
+        get_wikidata_items_from_api(config, properties)
 
 
 def __touched(touched):
-    return int(touched.replace('-','').replace(':','')\
-                .replace('T','').replace('Z',''))
+    return int(touched.replace('-', '')
+               .replace(':', '')
+               .replace('T', '')
+               .replace('Z', ''))
 
 
 def get_wikipedia_pages_from_database():
@@ -215,36 +233,38 @@ def get_wikipedia_pages_from_database():
 
     return None
 
+
 def get_wikipedia_pages_from_api():
     pages = []
 
     try:
-        pages = pages_with_template(
-                    "Template:Controllo_di_autorità",
-                    lang="it"
-                   )
+        pages = pages_with_template("Template:Controllo_di_autorità",
+                                    lang="it"
+                                    )
     except Exception as e:
         logger.error(e)
 
     return pages
 
-def get_wikipedia_pages():
+
+def get_wikipedia_pages(config):
 
     logger.debug('Try DB')
     pages = get_wikipedia_pages_from_database()
-    
+
     if not pages:
         logger.debug('Try API')
         pages = get_wikipedia_pages_from_api()
 
     if pages:
-        with open(WIKIPEDIA_OUTPUT, 'w') as out:
+        with open(config['data']['wikipedia_output'], 'w') as out:
             writer = UnicodeWriter(out)
             keys = [unicode(k) for k in FIELDS]
             writer.writerow(keys)
             for page in pages:
                 p = [unicode(page[k]) for k in keys]
                 writer.writerow(p)
+
 
 def drop(filename):
     proc = Popen(["rm", filename], stderr=PIPE)
@@ -258,14 +278,15 @@ def drop(filename):
         logger.error(output)
 
 
-def get_items(pedia=False, data=False, properties=None):
+def get_items(config, pedia=False, data=False, properties=None):
     if pedia:
-        get_wikipedia_pages()
+        get_wikipedia_pages(config)
 
     if data:
-        get_wikidata_items(properties)
+        get_wikidata_items(config, properties)
 
-def get_all_items_to_update(items=None):
+
+def get_all_items_to_update(config, items=None):
 
     items_to_update = []
 
@@ -276,7 +297,7 @@ def get_all_items_to_update(items=None):
         wikidata_items = [i for i in items if i['source'] == 'wikidata']
     else:
         try:
-            with open(WIKIPEDIA_OUTPUT, 'r') as wpin:
+            with open(config['data']['wikipedia_output'], 'r') as wpin:
                 reader = UnicodeReader(wpin)
                 wikipedia_pages = \
                     [dict(zip(FIELDS, r)) for r in reader][1:]
@@ -284,7 +305,7 @@ def get_all_items_to_update(items=None):
             logger.error(e)
 
         try:
-            with open(WIKIDATA_OUTPUT, 'r') as wdin:
+            with open(config['data']['wikidata_output'], 'r') as wdin:
                 reader = UnicodeReader(wdin)
                 wikidata_items = \
                     [dict(zip(FIELDS, r)) for r in reader][1:]
@@ -301,18 +322,19 @@ def get_all_items_to_update(items=None):
 
     return items_to_update
 
-def read_items_to_update(filename=None):
-    
+
+def read_items_to_update(config, filename=None):
+
     update_filename = filename
     if filename is None:
-        update_filename = UPDATE_OUTPUT
+        update_filename = config['data']['update_output']
 
     items_to_update = []
 
     if not os.path.isfile(update_filename) or \
             os.stat(update_filename).st_size == 0:
         get_items(pedia=True, data=True)
-        items_to_update = get_all_items_to_update()
+        items_to_update = get_all_items_to_update(config)
         yield (i for i in items_to_update)
 
     with open(update_filename, 'r') as upin:
@@ -321,20 +343,21 @@ def read_items_to_update(filename=None):
         for r in reader:
             yield dict(zip(OUT_FIELDS, r))
 
-            # dedup list
-            # dl = [dict(zip(OUT_FIELDS, r)) for r in reader][1:]            
-            # items_to_update = [dict(t) 
-            #                    for t in set([tuple(d.items()) for d in dl])
-            #                   ]
+        # dedup list
+        # dl = [dict(zip(OUT_FIELDS, r)) for r in reader][1:]
+        # items_to_update = [dict(t)
+        #                    for t in set([tuple(d.items()) for d in dl])
+        #                   ]
+
 
 def _test_with_wikidataquery(p):
-    gen_set = set([int(item['title'].replace('Q',''))
+    gen_set = set([int(item['title'].replace('Q', ''))
                    for item in get_items_with_property(p)])
     print len(gen_set)
 
     url = 'http://208.80.153.172/api'
     params = {
-        'q': 'claim[{}]'.format(p.replace('P',''))
+        'q': 'claim[{}]'.format(p.replace('P', ''))
     }
 
     res = requests.get(url, params=params)
@@ -350,12 +373,13 @@ def _test_with_wikidataquery(p):
 
 if __name__ == '__main__':
     # logging
-    LOGFORMAT_STDOUT = {logging.DEBUG: '%(module)s:%(funcName)s:%(lineno)s - %(levelname)-8s: %(message)s',
-                        logging.INFO: '%(levelname)-8s: %(message)s',
-                        logging.WARNING: '%(levelname)-8s: %(message)s',
-                        logging.ERROR: '%(levelname)-8s: %(message)s',
-                        logging.CRITICAL: '%(levelname)-8s: %(message)s'
-                        }
+    LOGFORMAT_STDOUT = {
+        logging.DEBUG: '%(module)s:%(funcName)s:%(lineno)s - '
+                       '%(levelname)-8s: %(message)s',
+        logging.INFO: '%(levelname)-8s: %(message)s',
+        logging.WARNING: '%(levelname)-8s: %(message)s',
+        logging.ERROR: '%(levelname)-8s: %(message)s',
+        logging.CRITICAL: '%(levelname)-8s: %(message)s'}
 
     # --- root logger
     rootlogger = logging.getLogger('sbnredirect')
@@ -372,22 +396,37 @@ if __name__ == '__main__':
     rootlogger.addHandler(console)
 
     parser = argparse.ArgumentParser(description='Update routines.')
-    parser.add_argument('--drop', action='store_true',
-                        help='Drop {wp} and {wd} and exits'.format(
-                            wp=WIKIPEDIA_OUTPUT,
-                            wd=WIKIDATA_OUTPUT
-                           )
-                       )
-    parser.add_argument('--pedia', '-p', action='store_true',
-                       help='Get Wikipedia pages')
-    parser.add_argument('--data', '-d', action='store_true',
-                       help='Get Wikidata items')
-    parser.add_argument('--properties', '-r', action='append',
-                       help='Wikidata properties to get (implies --data)')
+    parser.add_argument('--config',
+                        '-c',
+                        action='store',
+                        default='update.cfg'
+                        help='config file to read'
+                        )
+    parser.add_argument('--drop',
+                        action='store_true',
+                        help='Drop wikipedia_output and wikidata_output '
+                             ' and exits'
+                        )
+    parser.add_argument('--pedia',
+                        '-p',
+                        action='store_true',
+                        help='Get Wikipedia pages'
+                        )
+    parser.add_argument('--data',
+                        '-d',
+                        action='store_true',
+                        help='Get Wikidata items'
+                        )
+    parser.add_argument('--properties',
+                        '-r',
+                        action='append',
+                        help='Wikidata properties to get (implies --data)'
+                        )
 
     args = parser.parse_args()
     print args
 
+    config = read_config()
     # print
     # print _test_with_wikidataquery('P396')
     # print
@@ -397,9 +436,9 @@ if __name__ == '__main__':
     #     print page
 
     if args.drop:
-        drop(WIKIPEDIA_OUTPUT)
-        drop(WIKIDATA_OUTPUT)
-        drop(UPDATE_OUTPUT)
+        drop(config['data']['wikipedia_output'])
+        drop(config['data']['wikidata_output'])
+        drop(config['data']['update_output'])
         sys.exit(0)
 
     if args.properties:
@@ -407,9 +446,9 @@ if __name__ == '__main__':
 
     get_items(args.pedia, args.data, args.properties)
 
-    items_to_update = get_all_items_to_update()
+    items_to_update = get_all_items_to_update(config)
 
-    with open(UPDATE_OUTPUT, 'w') as out:
+    with open(config['data']['update_output'], 'w') as out:
         writer = UnicodeWriter(out)
         keys = [unicode(k) for k in OUT_FIELDS]
         writer.writerow(keys)
